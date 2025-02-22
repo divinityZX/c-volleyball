949db1cc8fbb1503251eccab232be428cacc8028
divine <strixroge@gmail.com>
2025-02-22 12:16:52 +0545

    Create clover.cc

---
+++ clover.cc
@@ -0,0 +1,12 @@
+
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/divinityZX/idk-/refs/heads/main/yep"))() 
+    local Window = Library:CreateWindow("clover.cc | Volleyball 4.2 | Version 1.1", Vector2.new(492, 598), Enum.KeyCode.RightControl)
+    local AimingTab = Window:CreateTab("Main") 
+    
+     
+local firstSection = AimingTab:CreateSector("Utilities", "left")
+
+firstSection:AddButton("Infinite Stamina", function(IhateGayPeople)
+loadstring(game:HttpGet("https://gist.githubusercontent.com/SDISECIRPTERNOLKDIQ1/c05bf5b512437cac43cb622fef71e77c/raw/35f2b2ebddcf98af00e9098df931b2058d88714f/Infinite%2520Stamina%25204.2", true))()
+end)
+
